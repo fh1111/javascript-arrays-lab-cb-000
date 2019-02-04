2ad266b0dbc64e2108899c457434094e5417064b
Fer Hernandez <fh.1111@gmail.com>
2019-02-04 03:32:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ function prependKitten(name){
   return [name,...kittens]
 }
 
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
 function removeLastKitten(){
   return kittens.slice(0, kittens.length - 1)
 }
